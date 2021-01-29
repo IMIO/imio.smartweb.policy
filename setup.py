e66f827e01fd33b9f056d674d8070662ba24b4a8
Christophe Boulanger <christophe.boulanger@imio.be>
2021-01-29 09:03:27 +0100

    Add eea.facetednavigation to project and override browserview to deactivate facetednavigation actions on imio.smartweb.Folder.

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
         'setuptools',
         # -*- Extra requirements: -*-
         'z3c.jbot',
+        'eea.facetednavigation',
         'plone.api>=1.8.4',
         'plone.restapi',
         'plone.app.dexterity',
---
+++ src/imio/smartweb/policy/browser/faceted/subtyper.py
@@ -0,0 +1,30 @@
+from eea.facetednavigation.subtypes.interfaces import IFacetedSubtyper
+from eea.facetednavigation.subtypes.subtyper import FacetedPublicSubtyper
+from zope.interface import implementer
+
+
+@implementer(IFacetedSubtyper)
+class FolderFacetedPublicSubtyper(FacetedPublicSubtyper):
+    """ Deactivate faceted navigation on imio.smartweb.Folder content type. """
+
+    @property
+    def can_enable(self):
+        """ See IFacetedSubtyper
+        """
+        return False
+
+    @property
+    def can_disable(self):
+        """ See IFacetedSubtyper
+        """
+        return False
+
+    def enable(self):
+        """ See IFacetedSubtyper
+        """
+        return False
+
+    def disable(self):
+        """ See IFacetedSubtyper
+        """
+        return False
