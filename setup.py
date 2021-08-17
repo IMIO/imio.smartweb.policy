72a259906021c2cecc0d5c882179933a9bec2dbf
Laurent Lasudry <lasudry@gmail.com>
2021-08-17 16:00:36 +0200

    Add missing collective.anysurfer dependency

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
         "z3c.jbot",
         "plone.api>=1.8.4",
         "plone.app.dexterity",
+        "collective.anysurfer",
         "collective.autopublishing",
         "collective.big.bang",
         "collective.messagesviewlet",
