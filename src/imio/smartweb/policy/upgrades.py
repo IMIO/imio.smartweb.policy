81d45db399e1b9489547be89c61f885d032d1a92
Laurent Lasudry <lasudry@gmail.com>
2021-11-04 16:38:52 +0100

    Use portal_setup instead of context

--- src/imio/smartweb/policy/upgrades.py
+++ src/imio/smartweb/policy/upgrades.py
@@ -10,5 +10,5 @@ PROFILEID = "profile-imio.smartweb.policy:default"
 def configure_first_official_release(context):
     portal_setup = api.portal.get_tool("portal_setup")
     portal_setup.runAllImportStepsFromProfile("profile-pas.plugins.imio:default")
-    context.runImportStepFromProfile(PROFILEID, "typeinfo")
-    context.runImportStepFromProfile(PROFILEID, "viewlets")
+    portal_setup.runImportStepFromProfile(PROFILEID, "typeinfo")
+    portal_setup.runImportStepFromProfile(PROFILEID, "viewlets")
