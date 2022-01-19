0204c377e1cc2fb40ead4fba73d9eb8ddb89ca5b
Christophe Boulanger <christophe.boulanger@imio.be>
2022-01-19 14:44:02 +0100

    Preparing release 1.0a10

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.smartweb.policy",
-    version="1.0a10.dev0",
+    version="1.0a10",
     description="Policies to setup imio.smartweb",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
