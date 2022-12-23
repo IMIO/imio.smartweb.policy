f1cba7b695b3496735d428eb73926646e17949d0
Laurent Lasudry <lasudry@gmail.com>
2022-12-23 10:25:24 +0100

    Preparing release 1.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="imio.smartweb.policy",
-    version="1.0.11.dev0",
+    version="1.1",
     description="Policies to setup imio.smartweb",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
